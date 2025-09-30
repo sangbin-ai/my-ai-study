758919a6fd48bff2d911cdfd43afaf6125423c17
이상빈 <dltkdqls12o@gmail.com>
2025-09-30 17:11:12 +0900

    Add Week2: List, Tuple, Slicing, List Comprehension practice

---
+++ Week 2/Python(문제풀이)/Slicing.py
@@ -0,0 +1,19 @@
+a = [10, 20, 30, 40, 50] 
+print(a[0:3]) #0=첫 번째 인덱스, 3= 3-1반째 인덱스
+
+alphabet = ["a", "b", "c", "d", "e"]
+print(alphabet[2:]) #2=3번째 인덱스, 빈칸은 끝자리까지
+
+text = "python"
+print(text[:3]) # 빈칸은 처음부터, 3= 2번째 인덱스 문자까지 출력
+
+text2 = "python"
+print(text2[4:]) # -2는 뒤에서 2번째 부터 출력
+
+# 리스트[start:end] → start부터 end-1까지 잘라냄
+
+# :end → 처음부터 end-1까지
+
+# start: → start부터 끝까지
+
+# - 인덱스 → 뒤에서부터
---
+++ Week 2/Python(실습)/실무 감각 문제.py
@@ -0,0 +1,23 @@
+ages = [15, 22, 35, 18, 40, 29, 13]#20세 이상 성인만 리스트 추출
+adult_ages = [age for age in ages if age >= 20]
+print(adult_ages)
+
+scores = [45, 67, 89, 32, 76, 90, 54]#60점 이상인 학생만 리스트 추출
+successful_student= [score for score in scores if score >=60]
+print(successful_student)
+
+roducts = ["apple", "banana", "airpods", "cherry", "avocado"] #a 들어가는 글자 리스트에서 출력
+result = [p for p in roducts if "a" in p]
+print(result)
+
+products = ["apple", "banana", "airpods", "cherry", "avocado", "orange"] #o 들어가는 글자 리스트에서 출력
+results=[ z for z in products if "o" in z]
+print(results)
+
+log = "2025-09-30 11:32:15 | User login success" #2025-09-30 글자 리스트에서 출력 *for문은 하나하나를 꺼내 바구니에 담는것이므로 전체 글자를 출력하는 여기선 for문 x
+Date = log[:10]
+print(Date)
+
+prices = [1000, 2500, 3000, 1500, 1200] # 현재 가격에서 부가세 10% 붙인 가격을 리스트에서 출력
+value = [int(p * 1.1) for p in prices]
+print(value)
